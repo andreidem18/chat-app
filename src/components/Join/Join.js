e14b3beaece3c01a4244880e97cea495d8da1fdf
Andrés Rojas <andres.02rojas@gmail.com>
2021-03-17 14:59:32 -0500

    join style 1.0

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,8 +1,10 @@
+import {body2} from './join.css'
 import {useDispatch} from 'react-redux';
 import {addRoom, logOut} from '../../actions/actionsGenerator';
 import {useHistory} from 'react-router-dom';
 import {useState} from 'react';
 import {useSelector} from 'react-redux';
+import { Button, Container, Form, Grid, Header, Label, Segment, } from "semantic-ui-react";
 
 const Join = () => {
 
@@ -21,10 +23,12 @@ const Join = () => {
     history.push('/');
     dispatch(logOut());
   }
+  
   return(
     <>
+        <div className="body2">
         <label>
-          Room
+          Roomooo
           <input type="text" 
             value={room} 
             onChange={(e) => setRoom(e.target.value)}/>
@@ -34,7 +38,86 @@ const Join = () => {
         </button>
         <button onClick={handleLogOut}>
           log out
-        </button>    
+        </button>   
+
+      <div>
+        <Container>
+        <Segment basic padded="very" />
+  
+        <Grid stackable>
+          <Grid.Row>
+            <Grid.Column only='computer' width={4}></Grid.Column>
+            <Grid.Column width={8}>
+              <Segment stacked raised padded="very">
+                <Header as="h2" textAlign="center" style={{color: "#0794D5"}}>
+                  Join to Room
+                  <Header.Subheader>
+                  Enter your data and the name of the room to access
+                  </Header.Subheader>
+                </Header>
+  
+                <Form error>
+                  <Form.Input
+                    fluid
+                    label="Email"
+                    placeholder="Email"
+                    type="email"
+                    value={""}
+                    onChange={""}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    label="Password"
+                    placeholder="Password"
+                    type="password"
+                    value={""}
+                    onChange={""}
+                    required
+                  />
+                  <Form.Input
+                    fluid
+                    label="Room"
+                    placeholder="Enter the room name"
+                    type="password"
+                    value={""}
+                    onChange={""}
+                    required
+                  />
+                  <Button
+                    animated="fade"
+                    color="google plus"
+                    onClick={""}
+                  >
+                    <Button.Content visible >Get into</Button.Content>
+                    <Button.Content hidden>Now!</Button.Content>
+                  </Button>
+  
+                  <Button
+                    color="blue"
+                    floated="right"
+                    onClick={() => history.push("/")}
+                  >
+                    log out
+                  </Button>
+                </Form>
+                <div style={{padding: "20px", textAlign:"center"}} >
+                <a href="#/signup">Create an account</a>
+                </div>
+  
+                <Label attached="top right" style={{background: "#0794D5", color: "white"}}>
+                  Join
+                </Label>
+              </Segment>
+              {""}
+            </Grid.Column>
+            <Grid.Column only='computer' width={4}></Grid.Column>
+          </Grid.Row>
+        </Grid>
+  
+        </Container>
+        </div>
+        </div>
     </>
   )
 }
